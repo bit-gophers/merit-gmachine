42fdb0a70b1a6a14a507d7d134f306efddd288f2
John Arundel <john@bitfieldconsulting.com>
2022-08-25 18:16:41 +0100

    implement enough for fibonacci
    Co-authored-by: Brian Hasden <bhasden@gmail.com>

--- gmachine.go
+++ gmachine.go
@@ -1,6 +1,86 @@
 // Package gmachine implements a simple virtual CPU, known as the G-machine.
 package gmachine
 
+import "fmt"
+
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
 const DefaultMemSize = 1024
+
+const (
+	OpHALT = iota
+	OpNOOP
+	OpINCA
+	OpDECA
+	OpSETA
+	OpSETI
+	OpDECI
+	OpJINZ
+	OpMVAY
+	OpADXY
+	OpMVAX
+	OpMVYA
+)
+
+type Word uint64
+
+type Machine struct {
+	Memory        []Word
+	A, I, P, X, Y Word
+}
+
+func New() *Machine {
+	return &Machine{
+		Memory: make([]Word, DefaultMemSize),
+	}
+}
+
+func (g *Machine) Run() error {
+	for {
+		// fmt.Printf("P: %d NextOp: %d A: %d I: %d X: %d Y: %d\n", g.P, g.Memory[g.P], g.A, g.I, g.X, g.Y)
+		op := g.Fetch()
+		switch op {
+		case OpHALT:
+			return nil
+		case OpNOOP:
+		case OpINCA:
+			g.A++
+		case OpDECA:
+			g.A--
+		case OpSETA:
+			g.A = g.Fetch()
+		case OpSETI:
+			g.I = g.Fetch()
+		case OpDECI:
+			g.I--
+		case OpJINZ:
+			if g.I != 0 {
+				g.P = g.Fetch()
+			} else {
+				g.P++
+			}
+		case OpMVAY:
+			g.Y = g.A
+		case OpADXY:
+			g.Y += g.X
+		case OpMVAX:
+			g.X = g.A
+		case OpMVYA:
+			g.A = g.Y
+		default:
+			return fmt.Errorf("unknown opcode %d", op)
+		}
+	}
+}
+
+func (g *Machine) Fetch() Word {
+	op := g.Memory[g.P]
+	g.P++
+	return op
+}
+
+func (g *Machine) RunProgram(data []Word) error {
+	copy(g.Memory, data)
+	g.P = 0
+	return g.Run()
+}
--- gmachine_test.go
+++ gmachine_test.go
@@ -1,8 +1,11 @@
 package gmachine_test
 
 import (
-	"gmachine"
 	"testing"
+
+	"gmachine"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestNew(t *testing.T) {
@@ -13,13 +16,172 @@ func TestNew(t *testing.T) {
 	if wantMemSize != gotMemSize {
 		t.Errorf("want %d words of memory, got %d", wantMemSize, gotMemSize)
 	}
-	var wantP uint64 = 0
+	var wantP gmachine.Word = 0
 	if wantP != g.P {
 		t.Errorf("want initial P value %d, got %d", wantP, g.P)
 	}
-	var wantMemValue uint64 = 0
+	var wantA gmachine.Word = 0
+	if wantA != g.A {
+		t.Errorf("want initial P value %d, got %d", wantA, g.A)
+	}
+
+	var wantMemValue gmachine.Word = 0
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
+	}
+}
+
+func TestHALT(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpHALT,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 1
+	got := g.P
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestNOOP(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpNOOP,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 2
+	got := g.P
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestINCA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpINCA,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 1
+	got := g.A
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestDECA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpSETA,
+		2,
+		gmachine.OpDECA,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 1
+	got := g.A
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestSubtract2From3Gives1(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpSETA,
+		3,
+		gmachine.OpDECA,
+		gmachine.OpDECA,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 1
+	got := g.A
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestSETA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpSETA,
+		5,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 5
+	got := g.A
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+	var wantP gmachine.Word = 3
+	gotP := g.P
+	if wantP != gotP {
+		t.Error(cmp.Diff(wantP, gotP))
+	}
+}
+
+// 1, 2, 3, 5, 8...
+func fib(n int) int {
+	result := 1
+	prev := 0
+	for i := 1; i <= n; i++ {
+		tmp := result + prev
+		prev = result
+		result = tmp
+	}
+	_ = prev
+	return result
+}
+
+func TestFib2(t *testing.T) {
+	t.Parallel()
+	want := 8
+	got := fib(5)
+	if want != got {
+		t.Error(cmp.Diff(want, got))
+	}
+}
+
+func TestFib(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	err := g.RunProgram([]gmachine.Word{
+		gmachine.OpINCA,
+		gmachine.OpSETI,
+		10,
+		gmachine.OpMVAY,
+		gmachine.OpADXY,
+		gmachine.OpMVAX,
+		gmachine.OpMVYA,
+		gmachine.OpDECI,
+		gmachine.OpJINZ,
+		3,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	var want gmachine.Word = 89
+	got := g.A
+	if want != got {
+		t.Error(cmp.Diff(want, got))
 	}
 }
